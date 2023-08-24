cc682ce3b5305958f531ee1c1d92bc2150830733
Tichin Wu <twu38@mail.ccsf.edu>
2023-08-24 14:49:56 -0700

    WIP TodoApp

---
+++ src/TodoApp.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+import TopTodo from "./TopTodo";
+import EditableTodoList from "./EditableTodoList";
+import TodoForm from "./TodoForm";
+
+/** App for managing a todo list.
+ *
+ * Props:
+ * - initialTodos: possible array of [ todo, ... ]
+ *
+ * State:
+ * - todos: array of [ todo, ... ]
+ *
+ * App -> TodoApp -> { TodoForm, EditableTodoList }
+ */
+
+function TodoApp({ initialTodos }) {
+  const [todos, setTodos] = useState(initialTodos);
+
+  /** add a new todo to list */
+  function create(newTodo) {
+
+    let newTD = { ...newTodo, id: uuid() };
+
+    setTodos(currTodos => [...currTodos, newTD]);
+  }
+
+  /** update a todo with updatedTodo */
+  function update(updatedTodo) {
+  }
+
+  /** delete a todo by id */
+  function remove(id) {
+  }
+
+  return (
+    <main className="TodoApp">
+      <div className="row">
+
+        <div className="col-md-6">
+          <EditableTodoList /> OR
+          <span className="text-muted">You have no todos.</span>
+        </div>
+
+        <div className="col-md-6">
+          (if no top todo, omit this whole section)
+          <section className="mb-4">
+            <h3>Top Todo</h3>
+            <TopTodo />
+          </section>
+
+          <section>
+            <h3 className="mb-3">Add Nü</h3>
+            <TodoForm handleSave={create} />
+          </section>
+        </div>
+
+      </div>
+    </main>
+  );
+}
+
+export default TodoApp;
---
+++ src/TodoApp.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import TodoApp from "./TodoApp";
+
+describe("productiv app", function () {
+  it("renders without crashing", function () {
+    render(<EditableTodoList />);
+  });
+
+  it("renders without crashing", function () {
+    render(<TopTodo />);
+  });
+
+  // it("contains expected title", function () {
+  //   const result = render(<App />);
+  //   expect(result.queryByText("Prøductïv")).toBeInTheDocument();
+  // });
+
+  // it("rendered quotes app", function () {
+  //   const result = render(<App />);
+  //   expect(result.queryByText("Click here for an inspirational quøte!")).toBeInTheDocument();
+  // });
+});
