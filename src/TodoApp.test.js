06ed4597d45af3636b4f73cf29ca7ec2f96b8dc9
Tichin Wu <twu38@mail.ccsf.edu>
2023-08-25 17:33:33 -0700

    got test edit worked

--- src/TodoApp.test.js
+++ src/TodoApp.test.js
@@ -5,44 +5,44 @@ import TodoApp from "./TodoApp";
 describe("productiv app", function () {
 
 
-  it("renders without crashing", function () {
-    render(<TodoApp initialTodos={[
-      {
-        id: 1,
-        title: "Code!",
-        description: "Write some code",
-        priority: 2,
-      }
-    ]} />);
-  });
+  // it("renders without crashing", function () {
+  //   render(<TodoApp initialTodos={[
+  //     {
+  //       id: 1,
+  //       title: "Code!",
+  //       description: "Write some code",
+  //       priority: 2,
+  //     }
+  //   ]} />);
+  // });
 
-  it("it can add new todo", function () {
-    const { queryByText, getByLabelText, debug, container } = render(<TodoApp initialTodos={[
-      {
-        id: 1,
-        title: "Code!",
-        description: "Write some code",
-        priority: 2,
-      }
-    ]} />);
+  // it("it can add new todo", function () {
+  //   const { queryByText, getByLabelText, debug, container } = render(<TodoApp initialTodos={[
+  //     {
+  //       id: 1,
+  //       title: "Code!",
+  //       description: "Write some code",
+  //       priority: 2,
+  //     }
+  //   ]} />);
 
-    debug(container);
+  //   debug(container);
 
-    expect(queryByText("make dinner")).not.toBeInTheDocument();
+  //   expect(queryByText("make dinner")).not.toBeInTheDocument();
 
-    const titleInput = getByLabelText("Title:");
-    const descriptionInput = getByLabelText("Description:");
-    const priorityInput = getByLabelText("Priority:");
-    const submitBtn = queryByText("Gø!");
+  //   const titleInput = getByLabelText("Title:");
+  //   const descriptionInput = getByLabelText("Description:");
+  //   const priorityInput = getByLabelText("Priority:");
+  //   const submitBtn = queryByText("Gø!");
 
-    fireEvent.change(titleInput, { target: { value: 'make dinner' } });
-    fireEvent.change(descriptionInput, { target: { value: 'make big dinner' } });
-    fireEvent.change(priorityInput, { target: { value: '3' } });
-    fireEvent.click(submitBtn);
+  //   fireEvent.change(titleInput, { target: { value: 'make dinner' } });
+  //   fireEvent.change(descriptionInput, { target: { value: 'make big dinner' } });
+  //   fireEvent.change(priorityInput, { target: { value: '3' } });
+  //   fireEvent.click(submitBtn);
 
-    expect(queryByText("make dinner")).toBeInTheDocument();
+  //   expect(queryByText("make dinner")).toBeInTheDocument();
 
-  });
+  // });
 
 
   it("it can update a todo", function () {
@@ -61,14 +61,23 @@ describe("productiv app", function () {
       }
     ]} />);
 
-    debug(container);
+    // debug(container);
 
     expect(queryByText("Make dinner!")).toBeInTheDocument();
 
+    // TODO: click edit button
+    const editBtn = container.querySelector('.EditableTodo-toggle-2');
+    fireEvent.click(editBtn);
     const titleInput = container.querySelector('#newTodo-title-2');
+
     const descriptionInput = container.querySelector("#newTodo-description-2");
     const priorityInput = container.querySelector("#newTodo-priority-2");
     const submitBtn = container.querySelector(".NewTodoForm-addBtn-2");
+
+
+    console.log('titleInput=', titleInput);
+    debug(container);
+    console.log('editBtn=', editBtn);
 
     fireEvent.change(titleInput, { target: { value: 'make lunch' } });
     fireEvent.change(descriptionInput, { target: { value: 'make big lunch' } });
